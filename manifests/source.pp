d7b5e5f8b80d6f1aeefa4eafddaeb9ed7eff0fe6
Rob Reynolds <ferventcoder@gmail.com>
2015-08-22 10:46:55 -0500

    (WIP)(PUP-3428) Configure sources

---
+++ manifests/source.pp
@@ -0,0 +1,22 @@
+define chocolatey::source (
+  $ensure      = 'present',
+  $source_name = $title,
+  $location    = $title,
+  $enable      = true,
+  $user_name   = undef,
+  $password    = undef
+) {
+
+  #todo: check choco version from custom choco_version fact
+
+  $_choco_exe_path = "${::choco_install_path}\\bin\\choco.exe"
+
+  #choco source list -r
+  #choco source add -r -n -s -u -p
+  #choco source remove -r -n
+  #choco source enable -r -n
+  #choco source disable -r -n
+
+  #if user/pass, we have to attempt to set every time to see if it is set
+
+}
---
+++ spec/defines/source_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+RSpec.describe 'chocolatey::source', :type => :define do
+  let (:title) { 'choco_source' }
+end
