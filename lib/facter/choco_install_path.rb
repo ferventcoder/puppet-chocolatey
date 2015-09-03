3bcb123ff95583a205e16e76ab1241dfac1dc12d
Marc Sutter <marc.sutter@swisscom.com>
2015-09-03 16:40:37 +0200

    add windows confine statement

--- lib/facter/choco_install_path.rb
+++ lib/facter/choco_install_path.rb
@@ -1,18 +1,16 @@
 Facter.add('choco_install_path') do
+  confine :kernel => 'windows'
   setcode do
+    require 'win32/registry'
 
-    if Puppet::Util::Platform.windows?
-      require 'win32/registry'
-
-      value = nil
-      begin
-        hive = Win32::Registry::HKEY_LOCAL_MACHINE
-        hive.open('SYSTEM\CurrentControlSet\Control\Session Manager\Environment', Win32::Registry::KEY_READ | 0x100) do |reg|
-          value = reg['ChocolateyInstall']
-        end
-      rescue Win32::Registry::Error => e
-        value = nil
+    value = nil
+    begin
+      hive = Win32::Registry::HKEY_LOCAL_MACHINE
+      hive.open('SYSTEM\CurrentControlSet\Control\Session Manager\Environment', Win32::Registry::KEY_READ | 0x100) do |reg|
+        value = reg['ChocolateyInstall']
       end
+    rescue Win32::Registry::Error => e
+      value = nil
     end
 
     value || 'C:\ProgramData\chocolatey'
